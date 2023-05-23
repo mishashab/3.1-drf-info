0989b902011daf5b0773b083d415a17ad80200c4
Mikhail <misha.shabalin@mail.ru>
2023-05-23 19:46:41 +0300

    Add models in models.py

--- measurement/models.py
+++ measurement/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
 
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.DecimalField(decimal_places=1)
+    created_at = models.DateTimeField()
