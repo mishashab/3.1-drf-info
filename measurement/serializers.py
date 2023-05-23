cc91d97a047b998bcb972729e75828264320b95e
Mikhail <misha.shabalin@mail.ru>
2023-05-23 21:38:57 +0300

    Add New MeasurementReadSerializer to out 2 objects/ Add 1 object in MeasurementSerializer for adding (id)

--- measurement/serializers.py
+++ measurement/serializers.py
@@ -5,11 +5,17 @@ from .models import Sensor, Measurement
 class MeasurementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
+        fields = ['sensor', 'temperature', 'created_at']
+
+
+class MeasurementReadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
         fields = ['temperature', 'created_at']
 
 
 class SensorDetailSerializer(serializers.ModelSerializer):
-    measurements = MeasurementSerializer(read_only=True, many=True)
+    measurements = MeasurementReadSerializer(read_only=True, many=True)
 
     class Meta:
         model = Sensor
