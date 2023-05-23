4bac11b2e87d81083c59c0eacd78aa476aa6413a
Mikhail <misha.shabalin@mail.ru>
2023-05-23 20:14:13 +0300

    setting.py back to start

--- smart_home/settings.py
+++ smart_home/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_smart_home',
-        'USER': 'postgres',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
