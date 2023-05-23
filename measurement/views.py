a354f7121f703ad1c2ac99053ecf59ef993619bc
Mikhail <misha.shabalin@mail.ru>
2023-05-23 20:05:59 +0300

    Add SensorDetailView and MesurementView in views.py

--- measurement/views.py
+++ measurement/views.py
@@ -1,2 +1,14 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, \
+    RetrieveUpdateAPIView
+from .models import Sensor, Measurement
+from .serializers import MeasurementSerializer, SensorDetailSerializer
+
+
+class SensorDetailView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all().prefetch_related('measurements')
+    serializer_class = SensorDetailSerializer
+
+
+class MeasurementsView(CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
