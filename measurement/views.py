b1e847c9944e621ddc907379b247787495c8a861
Mikhail <misha.shabalin@mail.ru>
2023-05-23 21:40:05 +0300

    Changes on debag in views.py and urls.py

--- measurement/views.py
+++ measurement/views.py
@@ -1,14 +1,19 @@
-from rest_framework.generics import CreateAPIView, ListCreateAPIView, \
-    RetrieveUpdateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, \
+    CreateAPIView
 from .models import Sensor, Measurement
 from .serializers import MeasurementSerializer, SensorDetailSerializer
 
 
-class SensorDetailView(RetrieveUpdateAPIView):
-    queryset = Sensor.objects.all().prefetch_related('measurements')
+class SensorView(RetrieveUpdateAPIView):
     serializer_class = SensorDetailSerializer
+    queryset = Sensor.objects.all()
+
+
+class SensorsView(ListCreateAPIView):
+    serializer_class = SensorDetailSerializer
+    queryset = Sensor.objects.all()
 
 
 class MeasurementsView(CreateAPIView):
-    queryset = Measurement.objects.all()
     serializer_class = MeasurementSerializer
+    queryset = Measurement.objects.all()
