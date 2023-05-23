d5318452d8d34390d8197b18fdffa42896696f02
Mikhail <misha.shabalin@mail.ru>
2023-05-23 21:40:19 +0300

    Changes on debag in views.py and urls.py

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SensorDetailView, MeasurementsView
+from .views import SensorsView, SensorView, MeasurementsView
 
 
 urlpatterns = [
-    path('sensors/', SensorDetailView.as_view()),
+    path('sensors/', SensorsView.as_view()),
+    path('sensors/<pk>/', SensorView.as_view()),
     path('measurements/', MeasurementsView.as_view())
 ]
