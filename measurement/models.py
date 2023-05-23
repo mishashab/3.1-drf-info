5b3d7d7002bdec362388c69e79d94f9c4aea5f64
Mikhail <misha.shabalin@mail.ru>
2023-05-23 19:53:32 +0300

    Update models.py Measerument(add auto_now, auto_now_add)

--- measurement/models.py
+++ measurement/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
-
 
 class Sensor(models.Model):
     name = models.CharField(max_length=20)
@@ -9,6 +7,9 @@ class Sensor(models.Model):
 
 
 class Measurement(models.Model):
-    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
-    temperature = models.DecimalField(decimal_places=1)
-    created_at = models.DateTimeField()
+    sensor = models.ForeignKey(Sensor,
+                               on_delete=models.CASCADE,
+                               related_name='measurements'
+                               )
+    temperature = models.DecimalField(max_digits=4, decimal_places=1)
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=True)
