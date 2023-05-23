8a6fb8128866a2e00f443ac1e0ff673905443400
Mikhail <misha.shabalin@mail.ru>
2023-05-23 20:08:55 +0300

    Add URL's in urls.py

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import SensorDetailView, MeasurementsView
+
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorDetailView.as_view()),
+    path('measurements/', MeasurementsView.as_view())
 ]
